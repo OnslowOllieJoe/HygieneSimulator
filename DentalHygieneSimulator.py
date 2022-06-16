0f17fced51cba7d273c8bf44a5ec3b76f94e0e4a
OnslowOllieJoe <ollie.joe@student.onslow.school.nz>
2022-06-17 00:16:41 +1200

    latest save

--- DentalHygieneSimulator.py
+++ DentalHygieneSimulator.py
@@ -7,6 +7,11 @@
 MAX_HP = 100
 SEPARATOR = "---------------------------------"
 SPACE_LENGTH = 15
+weaken_weapons = [
+               ["Toothbrush", 15],
+               ["Dental Floss", 15],
+               ["Mouthwash", 15]
+               ]
 
 
 class characters():
@@ -18,43 +23,43 @@ class characters():
 
 
 def user_and_enemy_info():
+    print(f"{SEPARATOR}\n")
+    print("            Player:\n")
+    print(f"    Health:          ({mc.health}/{MAX_HP})HP")
+    print(f"    Wallet:          ${mc.money}")
     print(f"""\n{SEPARATOR}\n
             Enemies:\n""")
     for i in range(len(current_enemy_list)):
         print(f"    {current_enemy_list[i].type}: " +
               (" " * (SPACE_LENGTH - (len(current_enemy_list[i].type)))) +
-              f"{str(current_enemy_list[i].health)}/{MAX_HP}")
-    print(f"{SEPARATOR}\n")
-    print("            Player:\n")
-    print(f"    Health:     {mc.health}/{MAX_HP}")
-    print(f"    Wallet:     ${mc.money}")
+              f"({str(current_enemy_list[i].health)}/{MAX_HP})HP")
+    
 
 
 def weapon_selection():
-    weaken_weapons = [
-               ["Tooth Brush", 15],
-               ["Dental Floss", 15],
-               ["Mouthwash", 15]
-               ]
-    print(SEPARATOR)
+    print("\n" + SEPARATOR)
     print("\n            Weapons:\n")
     for count, value in enumerate(weaken_weapons, start=1):
         print(f"    {count}: " + value[0])
     while True:
         try:
-            print(SEPARATOR)
-            chosen_weapon = int(input("\nPick your weapons number: ")) - 1
-            print(f"You have chosen {weaken_weapons[chosen_weapon][0]}!")
-            print("\n" +SEPARATOR)
-            return weaken_weapons[chosen_weapon][1]
+            print("\n" + SEPARATOR)
+            chosen_weapon = int(input("\nEnter a weapons number: "))
+            if chosen_weapon in range(1, len(weaken_weapons) + 1):
+                print(f"You have chosen {weaken_weapons[chosen_weapon - 1][0]}!")
+                print("\n" +SEPARATOR)
+                return weaken_weapons[chosen_weapon - 1][0]
+            else:
+                print("Please enter a number from {} to {}."
+                .format(1, len(weaken_weapons)))
         except:
-            print("    Sorry, that is not a valid choice.")
+            print("Sorry, that is not a valid choice.")
 
 
-mc = characters("Player", MAX_HP, 0, 0)
-plaque = characters("Plaque", MAX_HP, 15, 0)
-tartar = characters("Tartar", MAX_HP, 15, 0)
-tooth_decay = characters("Tooth Decay", MAX_HP, 15, 0)
+mc = characters("Player", 100, 0, 0)
+plaque = characters("Plaque", 100, 15, 0)
+tartar = characters("Tartar", 100, 15, 0)
+tooth_decay = characters("Tooth Decay", 100, 15, 0)
 current_enemy_list = [
               plaque, 
               tartar, 
