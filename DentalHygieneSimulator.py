f10e17e313413a9537ef1fc2a61195cdfffc6751
OCOllieJoe <ollie.joe@student.onslow.school.nz>
2022-08-11 13:12:31 +1200

    .

--- DentalHygieneSimulator.py
+++ DentalHygieneSimulator.py
@@ -554,7 +554,7 @@ def you_lose(round, fought, player):
     print(SEPARATOR)
     sleep(0.1)
     proceed()
-    menu()
+    menu(round, fought)
 
 
 def buy_health(player):
@@ -633,9 +633,9 @@ def battle(round, fought):
     enemy_one = characters(enemies_list[randint(0,
                            len(enemies_list) - 1)], 2, 3, 0)
     enemy_two = characters(enemies_list[randint(0,
-                           len(enemies_list) - 1)], 2, 3, 0)
+                           len(enemies_list) - 1)], 100, 3, 0)
     enemy_three = characters(enemies_list[randint(0,
-                             len(enemies_list) - 1)], 2, 3, 0)
+                             len(enemies_list) - 1)], 100, 3, 0)
     current = [enemy_one, enemy_two, enemy_three]
     user_info(player)
     dead = []
